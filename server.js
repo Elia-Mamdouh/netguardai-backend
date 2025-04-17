a81c6034a6635131622cb497ac8200977ad3f615
Elia-Mamdouh <elo.mamdouh@gmail.com>
2025-04-17 20:52:29 +0200

    Update server.js

--- server.js
+++ server.js
@@ -185,7 +185,7 @@ app.post("/generate-report", async (req, res) => {
 });
 
 // URL of the Python FAISS microservice
-const FAISS_URLconst FAISS_URL = "https://faiss-service.onrender.com/query";
+const FAISS_URL = "https://netguardai-faiss.onrender.com/query";
 
 app.post("/signup", async (req, res) => {
     try {
